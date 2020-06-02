c7077cfc93945a1114114da7d6a3093dcbaa9aab
BohdanNikolaychuk <nikolaychukbm@gmail.com>
2020-06-02 19:38:47 +0300

    11

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Student();
+
+            Console.WriteLine("Введіть інформацію про себе");
+            Console.Write($"Name: \t ");
+            p.Name = Console.ReadLine();
+            Console.Write($"Lastname: \t ");
+            p.LastName = Console.ReadLine();
+            Console.Write($"Group: \t \t ");
+            p.Group = Console.ReadLine();
+            Console.Write($"Year: \t ");
+            p.Year =int.Parse(Console.ReadLine());
+            Console.Write($"Adress: \t ");
+            p.Adress = Console.ReadLine();
+            Console.Write($"Passport: \t ");
+            p.Passport = Console.ReadLine();
+            Console.Write($"Age: \t ");
+            p.Age =int.Parse(Console.ReadLine());
+            Console.Write($"Telephone: \t ");
+            p.Telephone = int.Parse(Console.ReadLine());
+            Console.Write($"Rating: \t ");
+            p.Rating =int.Parse(Console.ReadLine());
+            StudentsRating(p.Rating);
+        }
+        public static int StudentsRating(int R)
+        {
+            if (R >= 90)
+            {
+                Console.WriteLine("Вітаємо відмінника");
+                return 1;
+            }
+
+            else if (R >= 75 && R < 90)
+            {
+                Console.WriteLine("Можна вчитися краще");
+                return 2;
+            }
+            else
+            {
+                Console.WriteLine("більше уваги приділяти навчанню");
+                return 3;
+            }
+            
+        }
+    }
+}
---
+++ ConsoleApp2/Student.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+   public class Student
+    {
+        private string name;
+        private string lastName;
+        private string group;
+        private string passport;
+        private int year;
+        private string adress;
+        private int age;
+        private int telephone;
+        private int rating;
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public string LastName
+        {
+            get => lastName;
+            set => lastName = value;
+        }
+        public string Group
+        {
+            get => group;
+            set => group = value;
+        }
+        public string Passport
+        {
+            get => passport;
+            set => passport = value;
+
+        }
+        public int Year
+        {
+            get => year;
+            set => year = value;
+        }
+        public string Adress
+        {
+            get => adress;
+            set => adress = value;
+        }
+        public int Age
+        {
+            get => age;
+            set => age = value;
+        }
+        public int Telephone
+        {
+            get => telephone;
+            set => telephone = value;
+        }
+        public int Rating
+        {
+            get => rating;
+            set => rating = value;
+        }
+        
+
+    }
+}
